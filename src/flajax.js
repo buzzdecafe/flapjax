c4ef65f537556285a9fc30a63162cf1d848db0d6
buzzdecafe <m_hur@yahoo.com>
2017-05-03 21:23:03 -0400

    get the build working

--- src/flajax.js
+++ src/flajax.js
@@ -1,19 +1,6 @@
 // module flajax
-//
 
 import * as Task from 'data.task';
-
-const onStateChange = xhr => function _onState() => {
-  if (xhr.readyState === xhr.DONE) {
-    xhr.removeEventListener('readystatechange', _onState, false)
-    
-    if (xhr.status >= 200 && xhr.status < 300) {
-      resolve(safeParse(xhr))
-    } else {
-      reject(safeParse(xhr))
-    }
-  }
-};
 
 const safeParse = xhr => {
   try {
@@ -25,12 +12,23 @@ const safeParse = xhr => {
 
 export function flajax(opts) {
   return new Task((reject, resolve) => {
+
+    const onStateChange = xhr => function _onState() {
+      if (xhr.readyState === xhr.DONE) {
+        xhr.removeEventListener('readystatechange', _onState, false);
+    
+        if (xhr.status >= 200 && xhr.status < 300) { resolve(safeParse(xhr));} 
+        else { reject(safeParse(xhr)); }
+      }
+    };
+
     var xhr = new XMLHttpRequest();
     xhr.open(type, opts.url, true);
     xhr.withCredentials = opts.hasOwnProperty('withCredentials');
     setHeaders(xhr, opts.headers);
     xhr.addEventListener('readystatechange', onStateChange(xhr), false);
     xhr.send();
+
   });
 };
 
