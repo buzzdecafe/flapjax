dcabd1d07ff5625d15193e756d14d204ef4d7510
buzzdecafe <m_hur@yahoo.com>
2017-05-03 21:16:04 -0400

    first whack at it

--- src/flajax.js
+++ src/flajax.js
@@ -1,13 +1,37 @@
 // module flajax
 //
 
-import Task from 'data.task';
+import * as Task from 'data.task';
 
+const onStateChange = xhr => function _onState() => {
+  if (xhr.readyState === xhr.DONE) {
+    xhr.removeEventListener('readystatechange', _onState, false)
+    
+    if (xhr.status >= 200 && xhr.status < 300) {
+      resolve(safeParse(xhr))
+    } else {
+      reject(safeParse(xhr))
+    }
+  }
+};
 
-
+const safeParse = xhr => {
+  try {
+    return JSON.parse(xhr.responsetext);
+  } catch (e) {
+    return xhr.responseText;
+  }
+}
 
 export function flajax(opts) {
-  
+  return new Task((reject, resolve) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open(type, opts.url, true);
+    xhr.withCredentials = opts.hasOwnProperty('withCredentials');
+    setHeaders(xhr, opts.headers);
+    xhr.addEventListener('readystatechange', onStateChange(xhr), false);
+    xhr.send();
+  });
 };
 
 
