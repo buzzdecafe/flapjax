291401ff66e398746f7fc159bfd1f1ba14a87a15
buzzdecafe <m_hur@yahoo.com>
2017-05-03 21:23:03 -0400

    get the build working

--- dist/flajax.bundle.js
+++ dist/flajax.bundle.js
@@ -362,10 +362,35 @@ var Task = Object.freeze({
 });
 
 // module flajax
-//
+
+const safeParse = xhr => {
+  try {
+    return JSON.parse(xhr.responsetext);
+  } catch (e) {
+    return xhr.responseText;
+  }
+};
 
 function flajax(opts) {
-  return new Task(() => 1, () => 2);
+  return new Task((reject, resolve) => {
+
+    const onStateChange = xhr => function _onState() {
+      if (xhr.readyState === xhr.DONE) {
+        xhr.removeEventListener('readystatechange', _onState, false);
+    
+        if (xhr.status >= 200 && xhr.status < 300) { resolve(safeParse(xhr));} 
+        else { reject(safeParse(xhr)); }
+      }
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(type, opts.url, true);
+    xhr.withCredentials = opts.hasOwnProperty('withCredentials');
+    setHeaders(xhr, opts.headers);
+    xhr.addEventListener('readystatechange', onStateChange(xhr), false);
+    xhr.send();
+
+  });
 }
 
 exports.flajax = flajax;
