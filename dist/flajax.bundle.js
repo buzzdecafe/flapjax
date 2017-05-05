e55f4050b39d37d285ee454cf7a598ccc172ae33
Michael Hurley <mhurley2@travelers.com>
2017-05-05 17:26:08 -0400

    add demo

--- dist/flajax.bundle.js
+++ dist/flajax.bundle.js
@@ -356,33 +356,42 @@ var index = task;
 
 // module flajax
 
-const safeParse = xhr => {
-  try {
-    return JSON.parse(xhr.responsetext);
-  } catch (e) {
-    return xhr.responseText;
-  }
+const setHeaders = (headers, xhr) => { 
+  Object.keys(headers).forEach(k => {
+    xhr.setRequestHeader(k, headers[k]);
+  });
+  return xhr; 
 };
 
-const setHeaders = (headers, xhr) => { return xhr; };
+const id = x => x;
 
-function flajax(opts) {
+// type opts = {
+//   url : String,
+//   method: String.
+//   withCredentials: Bool | undefined
+//   headers: { String : String }
+// }
+//
+// flajax :: (Object. (XHR -> XHR)) -> Task ((a -> ()), (b -> ())) -> ()
+function flajax(opts, dec) {
+  const decorator = dec || id;
   return new index((reject, resolve) => {
 
     const onStateChange = xhr => function _onState() {
       if (xhr.readyState === xhr.DONE) {
         xhr.removeEventListener('readystatechange', _onState, false);
     
-        if (xhr.status >= 200 && xhr.status < 300) { resolve(safeParse(xhr));} 
-        else { reject(safeParse(xhr)); }
+        if (xhr.status >= 200 && xhr.status < 300) { resolve(xhr);} 
+        else { reject(xhr); }
       }
     };
 
     var xhr = new XMLHttpRequest();
     xhr.open(opts.method, opts.url, true);
-    xhr.withCredentials = opts.hasOwnProperty('withCredentials');
+    xhr.withCredentials = !!opts.withCredentials;
     xhr = setHeaders(opts.headers, xhr);
     xhr.addEventListener('readystatechange', onStateChange(xhr), false);
+    xhr = decorator(xhr);
     xhr.send();
   });
 }
